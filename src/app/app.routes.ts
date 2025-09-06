209f17f8f461ee0ff15e79c44c80095fb95c7ea5
Nivaldo Hydalgo <nivaldohydalgo@gmail.com>
2025-09-05 22:08:12 -0300

    primeiros ajustes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './contents/home/home';
+import { About } from './contents/about/about';
+import { PageNotFound } from './contents/page-not-found/page-not-found';
+
+export const routes: Routes = [
+    { path: 'home', component: Home },
+    { path: 'about', component: About },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '**', component: PageNotFound },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Main } from './views/main/main';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+    selector: 'app-root',
+    imports: [Main],
+    templateUrl: './app.html',
+    styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('my-site');
+    protected readonly title = signal('my-site');
 }
